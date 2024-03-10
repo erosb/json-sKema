f253413e57cad972c83eabba50635532195c76fc
Bence Erős <ebence88@gmail.com>
2024-03-10 08:10:46 +0100

    making all tests in time.json pass

--- src/main/kotlin/com/github/erosb/jsonsKema/Format.kt
+++ src/main/kotlin/com/github/erosb/jsonsKema/Format.kt
@@ -2,19 +2,21 @@ package com.github.erosb.jsonsKema
 
 import org.apache.commons.validator.routines.EmailValidator
 import org.apache.commons.validator.routines.InetAddressValidator
-import java.lang.IllegalArgumentException
 import java.net.URI
 import java.net.URISyntaxException
-import java.time.Duration
-import java.time.LocalTime
+import java.text.ParsePosition
+import java.time.DateTimeException
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeFormatter.ISO_LOCAL_TIME
 import java.time.format.DateTimeFormatterBuilder
 import java.time.format.DateTimeParseException
 import java.time.format.ResolverStyle
 import java.time.temporal.ChronoField
-import java.util.UUID
+import java.time.temporal.TemporalAccessor
+import java.util.*
 import java.util.regex.Pattern
+
 
 typealias FormatValidator = (instance: IJsonValue, schema: FormatSchema) -> ValidationFailure?
 
@@ -99,11 +101,44 @@ internal val ipv6FormatValidator: FormatValidator = {inst, schema -> inst.maybeS
     }
 }}
 
+private val TIME_FORMATTER = DateTimeFormatterBuilder()
+    .parseCaseInsensitive()
+    .append(ISO_LOCAL_TIME)
+    .appendOffset("+HH:MM", "Z")
+    .parseLenient()
+    .toFormatter()
+
+private const val MAX_OFFSET_MIN = 24 * 60 - 1;
+private const val MIN_OFFSET_MIN = -MAX_OFFSET_MIN;
+
+// ported from https://github.com/networknt/json-schema-validator/blob/master/src/main/java/com/networknt/schema/format/TimeFormat.java
 internal val timeFormatValidator: FormatValidator = {inst, schema -> inst.maybeString { str ->
     try {
-        DateTimeFormatter.ISO_OFFSET_TIME.parse(str.value)
-        null
-    } catch (e: DateTimeParseException) {
+        val pos: Int = str.value.indexOf('Z')
+        if (-1 != pos && pos != str.value.length - 1) {
+            return@maybeString FormatValidationFailure(schema, str)
+        }
+        val accessor: TemporalAccessor = TIME_FORMATTER.parseUnresolved(str.value, ParsePosition(0))
+            ?: return@maybeString  FormatValidationFailure(schema, str)
+        val offsetMins = accessor.getLong(ChronoField.OFFSET_SECONDS) / 60
+        if (MAX_OFFSET_MIN < offsetMins || MIN_OFFSET_MIN > offsetMins) {
+            return@maybeString FormatValidationFailure(schema, str)
+        }
+        var hr = accessor.getLong(ChronoField.HOUR_OF_DAY) - offsetMins / 60
+        var min = accessor.getLong(ChronoField.MINUTE_OF_HOUR) - offsetMins % 60
+        val sec = accessor.getLong(ChronoField.SECOND_OF_MINUTE)
+        if (min < 0) {
+            --hr
+            min += 60
+        }
+        if (hr < 0) {
+            hr += 24
+        }
+        val isStandardTimeRange = sec <= 59 && min <= 59 && hr <= 23
+        val isSpecialCaseEndOfDay = sec == 60L && min == 59L && hr == 23L
+        return@maybeString if (isStandardTimeRange
+                || isSpecialCaseEndOfDay) null else FormatValidationFailure(schema, str)
+    } catch (e: DateTimeException) {
         FormatValidationFailure(schema, str)
     }
 }}
